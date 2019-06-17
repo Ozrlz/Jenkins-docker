2f54a39bf7e5d3cde3c47f20d5e1638d350f6c81
ozrlz <oscar.cucei@gmail.com>
2019-06-17 11:51:51 -0500

    [FIX] delete "-ce" on docker installation script URL

--- inst-docker.sh
+++ inst-docker.sh
@@ -3,7 +3,7 @@
 DOCKER_VERSION="18.09.0"
 
 mkdir -p /home/docker-inst/ && cd /home/docker-inst/
-curl https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}-ce.tgz -o docker-${DOCKER_VERSION}.tgz
+curl https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz -o docker-${DOCKER_VERSION}.tgz
 tar xzf docker-${DOCKER_VERSION}.tgz
 cp docker/docker /usr/bin/docker && chmod +x /usr/bin/docker && chown root:root /usr/bin/docker
 echo "jenkins ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
